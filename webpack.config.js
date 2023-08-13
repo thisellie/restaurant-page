f5aba4f36f6e22686be21654c3bf357a8654913e
Ron <81550623+thisellie@users.noreply.github.com>
2023-08-13 16:50:34 +0800

    Add packages and their configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[hash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+      favicon: './src/favicon.ico',
+    }),
+  ],
 }
+
