f49a2eb9716f207a7360a6e3f57f546d34114e81
Ron <81550623+thisellie@users.noreply.github.com>
2023-08-17 16:57:16 +0800

    Add sass package and config

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(s[ac]ss|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
